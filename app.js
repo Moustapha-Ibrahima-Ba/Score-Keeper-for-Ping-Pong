8e026b1e5656871837d4f9b823d755fa06732e33
Moustapha-Ibrahima-Ba <mr.moustapha.i.ba@gmail.com>
2022-12-15 12:53:24 +0000

    Version 1

---
+++ app.js
@@ -0,0 +1,57 @@
+// ---------- Select the elements ----------
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playTo')
+// ---------- Initialize the score ----------
+let isGameOver = false;
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+
+// ---------- Get the scoreLimit ----------
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+// ---------- Update the scores ----------
+p1Button.addEventListener('click', () => {
+    if (!isGameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
+        }
+        p1Display.textContent = p1Score;
+    }
+})
+
+p2Button.addEventListener('click', () => {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+            p1Display.classList.add('loser');
+            p2Display.classList.add('winner');
+        }
+        p2Display.textContent = p2Score;
+    }
+})
+
+// ---------- Reset the scores ----------
+resetButton.addEventListener('click', reset)
+
+// ---------- Function to reset the scores ----------
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p1Display.textContent = p1Score;
+    p2Score = 0;
+    p2Display.textContent = p2Score;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
